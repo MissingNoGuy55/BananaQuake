aa6fef76745434c86fd36f26f112dab670cee813
Missi <MissingNoGuy55@gmail.com>
2022-03-03 06:13:26 -0800

    Typo fix

--- WinQuake/cmd.cpp
+++ WinQuake/cmd.cpp
@@ -553,7 +553,7 @@ void	Cmd_AddCommand (char *cmd_name, xcommand_t function)
 		}
 	}
 
-	cmd = static_cast<cmd_function_s*>(g_MemCache->Hunk_Alloc (sizeof(cmd_function_t)));
+	cmd = static_cast<cmd_function_t*>(g_MemCache->Hunk_Alloc (sizeof(cmd_function_t)));
 	cmd->name = cmd_name;
 	cmd->function = function;
 	cmd->next = cmd_functions;
