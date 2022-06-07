416c6393560764ca103783486e71ce6f221ba48d
Missi <MissingNoGuy55@gmail.com>
2022-06-07 07:45:29 -0700

    Add missing files

---
+++ WinQuake/dropbox_ignore.ps1
@@ -0,0 +1,7 @@
+Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\debug_gl" -Stream com.dropbox.ignored -Value 1
+Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.pdb" -Stream com.dropbox.ignored -Value 1
+Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.obj" -Stream com.dropbox.ignored -Value 1
+Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.obj.enc" -Stream com.dropbox.ignored -Value 1
+Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.enc" -Stream com.dropbox.ignored -Value 1
+Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.log" -Stream com.dropbox.ignored -Value 1
+Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.idb" -Stream com.dropbox.ignored -Value 1
---
+++ WinQuake/server.cpp
@@ -0,0 +1,5 @@
+#include "quakedef.h"
+
+CQuakeServer::CQuakeServer()
+{
+}
