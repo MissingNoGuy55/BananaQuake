83cb257be9a80bcfdeaf2c2a584e4c0184129f6b
Missi <MissingNoGuy55@gmail.com>
2022-07-04 23:53:37 -0700

    Remove `<xmemory>` include from `quakedef.h`

--- WinQuake/quakedef.h
+++ WinQuake/quakedef.h
@@ -51,7 +51,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <setjmp.h>
 // #include <dsound.h>
 #include <windows.h>
-#include <xmemory>
 #include "SDL.h"
 
 #if defined(_WIN32) && !defined(WINDED)
