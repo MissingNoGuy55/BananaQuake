b10adff429c1b7395de73af0cd4313965426a8e6
Missi <MissingNoGuy55@gmail.com>
2023-01-07 06:59:32 -0800

    Updated `makezip_bquakebin.bat`

--- WinQuake/makezip_bquakebin.bat
+++ WinQuake/makezip_bquakebin.bat
@@ -1,2 +1,3 @@
+7z\7za a BananaQuake_bin.7z ./debug_gl_64/*.exe ./debug_gl/*.dll -r
 7z\7za a BananaQuake_bin.7z ./debug_gl/*.exe ./debug_gl/*.dll -r
 7z\7za a BananaQuake_bin.7z ./debug/*.exe ./debug/*.dll -r
