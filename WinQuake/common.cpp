e1a88c04294bcfacf5f621a870136dd860725bba
Missi <MissingNoGuy55@gmail.com>
2023-01-13 04:23:37 -0800

    Added `strlcpy.cpp` and moved `Q_strlcpy` from `common.cpp` to `strlcpy.cpp`

--- WinQuake/common.cpp
+++ WinQuake/common.cpp
@@ -205,31 +205,6 @@ void Q_strncpy (char *dest, const char *src, int count)
 		*dest++ = 0;
 }
 
-size_t Q_strlcpy(char* dst, const char* src, size_t siz)
-{
-	char* d = dst;
-	const char* s = src;
-	size_t n = siz;
-
-	/* Copy as many bytes as will fit */
-	if (n != 0) {
-		while (--n != 0) {
-			if ((*d++ = *s++) == '\0')
-				break;
-		}
-	}
-
-	/* Not enough room in dst, add NUL and traverse rest of src */
-	if (n == 0) {
-		if (siz != 0)
-			*d = '\0';		/* NUL-terminate dst */
-		while (*s++)
-			;
-	}
-
-	return(s - src - 1);	/* count does not include NUL */
-}
-
 int Q_strlen (const char *str)
 {
 	int             count;
