b9714633d48b1adf3dd2e6e5808fbbd7d3c5a305
Missi <MissingNoGuy55@gmail.com>
2023-01-15 08:27:34 -0800

    Updated `makezip_bquakebin.bat` to only include Release builds

--- WinQuake/makezip_bquakebin.bat
+++ WinQuake/makezip_bquakebin.bat
@@ -1,3 +1,6 @@
-7z\7za a BananaQuake_bin.7z ./debug_gl_64/*.exe ./debug_gl/*.dll -r
-7z\7za a BananaQuake_bin.7z ./debug_gl/*.exe ./debug_gl/*.dll -r
-7z\7za a BananaQuake_bin.7z ./debug/*.exe ./debug/*.dll -r
+7z\7za a x64.7z ./gl_release_64/*.exe ./gl_release_64/*.dll -r
+7z\7za a x86.7z ./gl_release/*.exe ./gl_release/*.dll -r
+7z\7za a x86.7z ./release/*.exe ./release/*.dll -r
+
+7z\7za a BananaQuake_bin.7z ./x86.7z
+7z\7za a BananaQuake_bin.7z ./x64.7z
