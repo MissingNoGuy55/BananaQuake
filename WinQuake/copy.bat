0bd39bd81504e094fb134b9d71542d831c64e463
Missi <MissingNoGuy55@gmail.com>
2023-04-21 04:34:19 -0700

    Updated `.gitignore`; Added `CMakeLists.txt`; added extra batch files; added `SciTechMGL.sln`

---
+++ WinQuake/copy.bat
@@ -0,0 +1,7 @@
+xcopy "./debug_gl/glbananaquake.exe" "%Quake_Path%" /f /y
+xcopy "./debug_gl/glbananaquake.pdb" "%Quake_Path%" /f /y
+xcopy "./debug_gl_64/glbananaquake64.exe" "%Quake_Path%" /f /y
+xcopy "./debug_gl_64/glbananaquake64.pdb" "%Quake_Path%" /f /y
+xcopy "./debug/softwarebananaquake.exe" "%Quake_Path%" /f /y
+xcopy "./debug/softwarebananaquake.pdb" "%Quake_Path%" /f /y
+pause
---
+++ WinQuake/copyrelease.bat
@@ -0,0 +1,7 @@
+xcopy "./release_gl/glbananaquake.exe" "%Quake_Path%" /f /y
+xcopy "./release_gl/glbananaquake.pdb" "%Quake_Path%" /f /y
+xcopy "./release_gl_64/glbananaquake64.exe" "%Quake_Path%" /f /y
+xcopy "./release_gl_64/glbananaquake64.pdb" "%Quake_Path%" /f /y
+xcopy "./release/softwarebananaquake.exe" "%Quake_Path%" /f /y
+xcopy "./release/softwarebananaquake.pdb" "%Quake_Path%" /f /y
+pause
