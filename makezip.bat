af7f6092fe1f56ab39769642589ac84790e6b313
Missi <MissingNoGuy55@gmail.com>
2023-04-27 05:16:46 -0700

    Updated `makezip.bat`

--- makezip.bat
+++ makezip.bat
@@ -1,1 +1,1 @@
-WinQuake\7z\7za a BananaQuake_src.7z *.cpp *.h *.s *.c *.sln *.dsp *.dsw *.bsc *.vcxproj *makefile *Makefile.linuxi386 *.solaris *.bat *.gif *.ico *.rc *.sh -r
+WinQuake\7z\7za a BananaQuake_src.7z CMakeLists.txt *.q1 *.q2 *.cpp *.h *.s *.c *.sln *.dsp *.dsw *.bsc *.vcxproj *makefile *Makefile.linuxi386 *.solaris *.bat *.gif *.ico *.rc *.sh -r
