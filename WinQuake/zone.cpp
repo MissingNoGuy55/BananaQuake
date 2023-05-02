cb2df5f1d271425e4ce49f0523f886b330e30568
Missi <MissingNoGuy55@gmail.com>
2023-05-02 10:18:52 -0700

    Updated `zone.cpp` (added comments at the top of file)

--- WinQuake/zone.cpp
+++ WinQuake/zone.cpp
@@ -17,7 +17,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// Z_zone.c
+// zone.cpp
+
+// Missi: most functions moved to zone.h due to turning most of the memory into template classes (5/2/2023)
 
 #include "quakedef.h"
 
