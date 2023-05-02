c9bcf4eda0eee7f9d941efad66015d89997b6f2d
Missi <MissingNoGuy55@gmail.com>
2023-05-02 04:55:30 -0700

    Updated `maketar.sh`

--- maketar.sh
+++ maketar.sh
@@ -2,7 +2,7 @@
 
 # -name "*.lib" -o -name "*.dll"
 
-find ./ -name "*.so" -o -name "*.a" -o -name "*.q1" -o -name "*.q2" -o -name "*.cpp" -o -name "*.h" -o -name "*.H" -o -name "*.c" -o -name "*.s" -o -name "*.sln" -o -name "*.dsp" -o -name "*.dsw" -o -name "*.vcxproj" -o -name "*Makefile" -o -name "*Makefile.linuxi386" -o -name "*.Solaris" -o -name "*.sh" -o -name "*.bat" -o -name "*.gif" -o -name "*.ico" -o -name "*.txt" -o -name "*.rc" -o -name "*.cmake" -name "*.cmake.in" | tar cfv bananaquake_src.tar -T -
+find ./ -name "*.so" -o -name "*.a" -o -name "*.q1" -o -name "*.q2" -o -name "*.cpp" -o -name "*.h" -o -name "*.H" -o -name "*.c" -o -name "*.s" -o -name "*.sln" -o -name "*.dsp" -o -name "*.dsw" -o -name "*.vcxproj" -o -name "*Makefile" -o -name "*Makefile.linuxi386" -o -name "*Makefile.cygwin" -o -name "*.Solaris" -o -name "*.sh" -o -name "*.bat" -o -name "*.gif" -o -name "*.ico" -o -name "*.txt" -o -name "*.rc" -o -name "*.cmake" -name "*.cmake.in" | tar cfv bananaquake_src.tar -T -
 
 find ./WinQuake/sdl2 -name "*" | tar ufv bananaquake_src.tar -T -
 find ./WinQuake/vorbis-svn -name "*" | tar ufv bananaquake_src.tar -T -
