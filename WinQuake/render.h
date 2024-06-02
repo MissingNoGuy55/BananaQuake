f714c015de14d90e6d298e25b79b5a0fc16aff46
Missi <MissingNoGuy55@gmail.com>
2024-06-02 15:40:46 -0700

    Changed `MAX_LIGHTMAPS` to 512

--- WinQuake/render.h
+++ WinQuake/render.h
@@ -41,7 +41,7 @@ constexpr short BLOCK_HEIGHT = 256;
 constexpr short LMBLOCK_WIDTH = 256;
 constexpr short LMBLOCK_HEIGHT = 256;
 
-constexpr short MAX_LIGHTMAPS = 64;
+constexpr short MAX_LIGHTMAPS = 512;		// Missi: was 64 (6/2/2024)
 
 //=============================================================================
 
