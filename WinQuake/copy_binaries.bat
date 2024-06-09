68c0bbce1b2f02a6f7d60fda15b25831934b877e
Missi <MissingNoGuy55@gmail.com>
2024-06-09 01:25:30 -0700

    Updated `copy_binaries.bat`

--- WinQuake/copy_binaries.bat
+++ WinQuake/copy_binaries.bat
@@ -2,4 +2,4 @@ copy .\codecs\x64\*.dll .\
 copy .\codecs\x64\*.dll .\build\Debug\
 copy .\codecs\x64\*.dll .\build\Release\
 copy .\codecs\x64\*.dll .\build\MinSizeRel\
-pause
+copy .\codecs\x64\*.dll .\build\RelWithDebInfo\
