b47fc14c6a69e3b9c41ea220b09a34def677e1e9
Missi <MissingNoGuy55@gmail.com>
2024-06-29 12:21:38 -0700

    Removed another unused script

--- WinQuake/dropbox_ignore.ps1
+++
@@ -1,9 +0,0 @@
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\debug_gl" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\.vs" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\.vscode" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.pdb" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.obj" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.obj.enc" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.enc" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.log" -Stream com.dropbox.ignored -Value 1
-Set-Content -path "D:\Dropbox\private stuff\game projects\QuakeCPP\WinQuake\*.idb" -Stream com.dropbox.ignored -Value 1
