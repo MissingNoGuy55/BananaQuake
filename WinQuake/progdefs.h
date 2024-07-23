d78a9ea40e1f64931caed1ce8f8d8c347664e028
Missi <MissingNoGuy55@gmail.com>
2024-07-23 15:32:30 -0700

    Added progdefs.bquake

--- WinQuake/progdefs.h
+++ WinQuake/progdefs.h
@@ -19,6 +19,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #ifdef QUAKE2
 #include "progdefs.q2"
+#elif BQUAKE
+#include "progdefs.bquake"
 #else
 #include "progdefs.q1"
 #endif
