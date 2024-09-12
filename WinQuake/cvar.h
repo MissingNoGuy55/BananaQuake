31416e92aac488d23d41e248c8ffe5e9c2f4396f
Stephen Schmiedeberg <missingnoguy55@gmail.com>
2024-09-12 00:43:39 -0700

    Updated copyright info (again)

--- WinQuake/cvar.h
+++ WinQuake/cvar.h
@@ -2,7 +2,7 @@
 
 /*
 Copyright (C) 1996-1997 Id Software, Inc.
-Copyright (C) 2021-2024 Stephen "Missi" Schimedeberg
+Copyright (C) 2021-2024 Stephen "Missi" Schmiedeberg
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
--- WinQuake/sys_win.cpp
+++ WinQuake/sys_win.cpp
@@ -1,6 +1,6 @@
 /*
 Copyright (C) 1996-1997 Id Software, Inc.
-Copyright (C) 2021-2024 Stephen "Missi" Schimedeberg
+Copyright (C) 2021-2024 Stephen "Missi" Schmiedeberg
 Copyright (C) 2021-2024 Stephen "Missi" Schmiedeberg
 
 This program is free software; you can redistribute it and/or
